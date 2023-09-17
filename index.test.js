acba5fbae2aea71ee28816f5fdedbe8dda6566c3
Vinson Chuong <vinsonchuong@gmail.com>
2023-09-17 14:08:21 -0700

    test(tls): Ensure that connecting via TLS works

--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 import test from 'ava'
 import {startServer, stopServer, compose, sendRequest} from 'passing-notes'
 import WebSocket from 'ws'
+import makeCert from 'make-cert'
 import serveWebSocket from './index.js'
 
 test('accepting WebSocket requests', async (t) => {
@@ -35,9 +36,43 @@ test('accepting WebSocket requests', async (t) => {
   t.is(message.toString(), 'Pong')
 })
 
+test('accepting WebSocket requests over TLS', async (t) => {
+  const {key, cert} = makeCert('localhost')
+
+  const server = await startServer(
+    {port: 10_001, cert, key},
+    compose(
+      serveWebSocket(async (ws) => {
+        const message = await new Promise((resolve) => {
+          ws.once('message', resolve)
+        })
+        t.is(message.toString(), 'Ping')
+
+        ws.send('Pong')
+      }),
+      () => () => ({status: 404}),
+    ),
+  )
+  t.teardown(() => {
+    stopServer(server)
+  })
+
+  const ws = new WebSocket('wss://localhost:10001', {rejectUnauthorized: false})
+  await new Promise((resolve) => {
+    ws.once('open', resolve)
+  })
+
+  ws.send('Ping')
+
+  const message = await new Promise((resolve) => {
+    ws.once('message', resolve)
+  })
+  t.is(message.toString(), 'Pong')
+})
+
 test('rejecting invalid WebSocket requests', async (t) => {
   const server = await startServer(
-    {port: 10_001},
+    {port: 10_002},
     compose(
       serveWebSocket(async (ws) => {
         ws.send('Hello World!')
@@ -52,7 +87,7 @@ test('rejecting invalid WebSocket requests', async (t) => {
   t.like(
     await sendRequest({
       method: 'GET',
-      url: 'http://localhost:10001',
+      url: 'http://localhost:10002',
     }),
     {
       status: 404,
@@ -62,7 +97,7 @@ test('rejecting invalid WebSocket requests', async (t) => {
   t.like(
     await sendRequest({
       method: 'GET',
-      url: 'http://localhost:10001',
+      url: 'http://localhost:10002',
       headers: {
         Connection: 'Upgrade',
         Upgrade: 'websocket',
@@ -76,7 +111,7 @@ test('rejecting invalid WebSocket requests', async (t) => {
   t.like(
     await sendRequest({
       method: 'GET',
-      url: 'http://localhost:10001',
+      url: 'http://localhost:10002',
       headers: {
         Connection: 'Upgrade',
         Upgrade: 'websocket',
@@ -91,7 +126,7 @@ test('rejecting invalid WebSocket requests', async (t) => {
   t.like(
     await sendRequest({
       method: 'GET',
-      url: 'http://localhost:10001',
+      url: 'http://localhost:10002',
       headers: {
         Connection: 'Upgrade',
         Upgrade: 'websocket',
